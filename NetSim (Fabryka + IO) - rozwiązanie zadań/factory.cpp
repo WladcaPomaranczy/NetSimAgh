2ec67f436ac2baf85c7ba69c2b56899a67850bba
Piotr Ficek <piotrficek@student.agh.edu.pl>
2026-01-22 17:59:49 +0100

    zmiany w facory.cpp

--- NetSim (Fabryka + IO) - rozwiązanie zadań/factory.cpp
+++ NetSim (Fabryka + IO) - rozwiązanie zadań/factory.cpp
@@ -1,1 +1,24 @@
 #include "factory.hxx"
+////////////////////////////////////////////////////////
+#include <map>
+enum class NodeColor { UNVISITED, VISITED, VERIFIED };
+
+bool is_network_consistent() const
+{
+    /* Użyj algorytmu DFS do przejścia grafu, dla każdej rampy. */
+    std::map<const PackageSender*, NodeColor>color;
+
+    for(const auto& ramp : ramps_) {
+        color[&ramp]= NodeColor::UNVISITED
+    }
+    for(const auto& work : workers_) {
+        color[&ramp]= NodeColor::UNVISITED
+
+    }
+
+
+}
+
+///////////////////////////
+
+
