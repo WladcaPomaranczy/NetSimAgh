66c0c0bb960909caf6c2192b2bbac20316c016ce
Piotr Ficek <piotrficek@student.agh.edu.pl>
2026-01-29 12:39:20 +0100

    factory changes

--- NetSim (Fabryka + IO) - rozwiązanie zadań/factory.cpp
+++ NetSim (Fabryka + IO) - rozwiązanie zadań/factory.cpp
@@ -1,24 +1,138 @@
 #include "factory.hxx"
-////////////////////////////////////////////////////////
+#include "nodes.hxx"
+
 #include <map>
-enum class NodeColor { UNVISITED, VISITED, VERIFIED };
+#include <stdexcept>
 
-bool is_network_consistent() const
+//FUNKCJA POMOCNICZA DFS
+bool has_reachable_storehouse(
+    const PackageSender* sender,
+    std::map<const PackageSender*, NodeColor>& colors)
 {
-    /* Użyj algorytmu DFS do przejścia grafu, dla każdej rampy. */
-    std::map<const PackageSender*, NodeColor>color;
+    if (colors[sender] == NodeColor::VERIFIED)
+        return true;
 
-    for(const auto& ramp : ramps_) {
-        color[&ramp]= NodeColor::UNVISITED
+    if (colors[sender] == NodeColor::VISITED)
+        return false;
+
+    colors[sender] = NodeColor::VISITED;
+
+    const auto& prefs = sender->receiver_preferences().get_preferences();
+    if (prefs.empty())
+        throw std::logic_error("Brak odbiorcow");
+
+    for (const auto& [receiver, prob] : prefs) {
+        if (receiver->get_receiver_type() == ReceiverType::STOREHOUSE) {
+            colors[sender] = NodeColor::VERIFIED;
+            return true;
+        }
+
+        // receiver = Worker (czyli też PackageSender)
+        auto next_sender = dynamic_cast<const PackageSender*>(receiver);
+        if (!next_sender) continue;
+
+        if (has_reachable_storehouse(next_sender, colors)) {
+            colors[sender] = NodeColor::VERIFIED;
+            return true;
+        }
     }
-    for(const auto& work : workers_) {
-        color[&ramp]= NodeColor::UNVISITED
 
-    }
-
-
+    return false;
 }
 
-///////////////////////////
+//FACTORY
+bool Factory::is_consistent() const
+{
+    std::map<const PackageSender*, NodeColor> colors;
 
+    for (const auto& r : ramps_)
+        colors[&r] = NodeColor::UNVISITED;
 
+    for (const auto& w : workers_)
+        colors[&w] = NodeColor::UNVISITED;
+
+    try {
+        for (const auto& r : ramps_) {
+            if (!has_reachable_storehouse(&r, colors))
+                return false;
+        }
+    } catch (...) {
+        return false;
+    }
+
+    return true;
+}
+
+void Factory::do_deliveries(Time t)
+{
+    for (auto& r : ramps_)
+        r.deliver_goods(t);
+}
+
+void Factory::do_package_passing()
+{
+    for (auto& w : workers_)
+        w.send_package();
+}
+
+void Factory::do_work(Time t)
+{
+    for (auto& w : workers_)
+        w.do_work(t);
+}
+
+//ADD
+void Factory::add_ramp(Ramp&& ramp)
+{
+    ramps_.add(std::move(ramp));
+}
+
+void Factory::add_worker(Worker&& worker)
+{
+    workers_.add(std::move(worker));
+}
+
+void Factory::add_storehouse(Storehouse&& storehouse)
+{
+    storehouses_.add(std::move(storehouse));
+}
+
+//FIND
+Ramp* Factory::find_ramp_by_id(ElementID id)
+{
+    auto it = ramps_.find_by_id(id);
+    return it == ramps_.end() ? nullptr : &(*it);
+}
+
+Worker* Factory::find_worker_by_id(ElementID id)
+{
+    auto it = workers_.find_by_id(id);
+    return it == workers_.end() ? nullptr : &(*it);
+}
+
+Storehouse* Factory::find_storehouse_by_id(ElementID id)
+{
+    auto it = storehouses_.find_by_id(id);
+    return it == storehouses_.end() ? nullptr : &(*it);
+}
+
+//REMOVE
+void Factory::remove_ramp(ElementID id)
+{
+    remove_receiver(workers_, id);
+    ramps_.remove_by_id(id);
+}
+
+void Factory::remove_worker(ElementID id)
+{
+    remove_receiver(workers_, id);
+    remove_receiver(ramps_, id);
+    workers_.remove_by_id(id);
+}
+
+void Factory::remove_storehouse(ElementID id)
+{
+    remove_receiver(workers_, id);
+    remove_receiver(ramps_, id);
+    storehouses_.remove_by_id(id);
+}
--- NetSim (Fabryka + IO) - rozwiązanie zadań/factory.hxx
+++ NetSim (Fabryka + IO) - rozwiązanie zadań/factory.hxx
@@ -1,24 +1,89 @@
-#include <map>
+#ifndef FACTORY_HXX
+#define FACTORY_HXX
 
-class PackageSender;
+#include <list>
+#include <algorithm>
 
+#include "types.hxx"
 #include "nodes.hxx"
 
+//NodeCollection
+template <class Node>
+class NodeCollection {
+public:
+    using container_t = std::list<Node>;
+    using iterator = typename container_t::iterator;
+    using const_iterator = typename container_t::const_iterator;
 
-enum class NodeColor { 
-    UNVISITED, 
-    VISITED, 
-    VERIFIED 
+    iterator begin() { return nodes_.begin(); }
+    iterator end() { return nodes_.end(); }
+    const_iterator begin() const { return nodes_.begin(); }
+    const_iterator end() const { return nodes_.end(); }
+    const_iterator cbegin() const { return nodes_.cbegin(); }
+    const_iterator cend() const { return nodes_.cend(); }
+
+    void add(Node&& node) {
+        nodes_.push_back(std::move(node));
+    }
+
+    iterator find_by_id(ElementID id) {
+        return std::find_if(
+            nodes_.begin(), nodes_.end(),
+            [id](const Node& n) { return n.get_id() == id; }
+        );
+    }
+
+    const_iterator find_by_id(ElementID id) const {
+        return std::find_if(
+            nodes_.cbegin(), nodes_.cend(),
+            [id](const Node& n) { return n.get_id() == id; }
+        );
+    }
+
+    void remove_by_id(ElementID id) {
+        auto it = find_by_id(id);
+        if (it != nodes_.end())
+            nodes_.erase(it);
+    }
+
+private:
+    container_t nodes_;
 };
 
-
-bool has_reachable_storehouse(
-    const PackageSender* sender, 
-    std::map<const PackageSender*, 
-    NodeColor>& node_colors);
-
-
+//Factory
 class Factory {
 public:
+    //consistency
     bool is_consistent() const;
+
+    //simulation
+    void do_deliveries(Time t);
+    void do_package_passing();
+    void do_work(Time t);
+
+    //ramps
+    void add_ramp(Ramp&& ramp);
+    void remove_ramp(ElementID id);
+    Ramp* find_ramp_by_id(ElementID id);
+
+    //workers
+    void add_worker(Worker&& worker);
+    void remove_worker(ElementID id);
+    Worker* find_worker_by_id(ElementID id);
+
+    //storehouses
+    void add_storehouse(Storehouse&& storehouse);
+    void remove_storehouse(ElementID id);
+    Storehouse* find_storehouse_by_id(ElementID id);
+
+private:
+    NodeCollection<Ramp> ramps_;
+    NodeCollection<Worker> workers_;
+    NodeCollection<Storehouse> storehouses_;
+
+    //helper do czyszczenia połączeń (zrobimy później)
+    template <class Node>
+    void remove_receiver(NodeCollection<Node>& collection, ElementID id);
 };
+
+#endif
